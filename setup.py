4c6cdbee7daceffcbf17dc561469c653c3ed798f
Marcus Bloice <marcus.bloice@medunigraz.at>
2016-03-15 19:45:22 +0100

    Setup files edited. Tag 0.1 added.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
   author = 'Marcus D. Bloice',
   author_email = 'marcus.bloice@medunigraz.at',
   url = 'https://github.com/mdbloice/Augmentor', # use the URL to the github repo
-  download_url = 'https://github.com/mdbloice/Augmentor', # I'll explain this in a second
+  download_url = 'https://github.com/mdbloice/Augmentor/tarball/0.1', # I'll explain this in a second
   keywords = ['image', 'augmentation', 'generation', 'machine', 'learning'], # arbitrary keywords
   classifiers = [],
 )
