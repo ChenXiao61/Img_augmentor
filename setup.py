bf3fb58222ddde4479d28d4c04d30c18b902d2a6
Marcus Bloice <marcus.bloice@medunigraz.at>
2016-03-15 19:40:16 +0100

    Added basic info to setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'Augmentor',
+  packages = ['Augmentor'], # this must be the same as the name above
+  version = '0.1',
+  description = 'Image augmentation library for Machine Learning',
+  author = 'Marcus D. Bloice',
+  author_email = 'marcus.bloice@medunigraz.at',
+  url = 'https://github.com/mdbloice/Augmentor', # use the URL to the github repo
+  download_url = 'https://github.com/mdbloice/Augmentor', # I'll explain this in a second
+  keywords = ['image', 'augmentation', 'generation', 'machine', 'learning'], # arbitrary keywords
+  classifiers = [],
+)
