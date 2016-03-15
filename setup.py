6fe61afb0387bda2952130c35f4501539866282a
Marcus Bloice <marcus.bloice@medunigraz.at>
2016-03-15 20:01:39 +0100

    Edited the CLASSIFIERS.

--- setup.py
+++ setup.py
@@ -9,5 +9,22 @@ setup(
   url = 'https://github.com/mdbloice/Augmentor', # use the URL to the github repo
   download_url = 'https://github.com/mdbloice/Augmentor/tarball/0.1', # I'll explain this in a second
   keywords = ['image', 'augmentation', 'generation', 'machine', 'learning'], # arbitrary keywords
-  classifiers = [],
+  CLASSIFIERS = [
+      "Development Status :: 1 - Beta",
+      "Intended Audience :: Developers",
+      "Natural Language :: English",
+      "License :: OSI Approved :: MIT License",
+      "Operating System :: OS Independent",
+      "Programming Language :: Python",
+      "Programming Language :: Python :: 2",
+      "Programming Language :: Python :: 2.6",
+      "Programming Language :: Python :: 2.7",
+      "Programming Language :: Python :: 3",
+      "Programming Language :: Python :: 3.3",
+      "Programming Language :: Python :: 3.4",
+      "Programming Language :: Python :: 3.5",
+      "Programming Language :: Python :: Implementation :: CPython",
+      "Programming Language :: Python :: Implementation :: PyPy",
+      "Topic :: Software Development :: Libraries :: Python Modules",
+  ],
 )
