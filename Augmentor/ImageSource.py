c5d95e62fba7510462100948fc6e8db5b49f7952
Marcus Bloice <marcus.bloice@medunigraz.at>
2016-03-18 10:11:42 +0100

    Added scan function.

--- Augmentor/ImageSource.py
+++ Augmentor/ImageSource.py
@@ -2,7 +2,11 @@ class ImageSource(object):
 
     def __init__(self, rootpath='.'):
         self.rootpath = rootpath
+        
+        scan(rootpath)
 
+    def scan(pathToScan):
+        print "Scanning " + pathToScan + " folder."
 
     def summary(self):
         print "The current directory is " + str(rootpath)
