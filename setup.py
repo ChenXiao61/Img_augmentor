8800ccf82dc925ca79a17c29f0f05e73e8e120c6
Marcus Bloice <marcus.bloice@medunigraz.at>
2016-08-03 14:30:11 +0200

    PyPI

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ from setuptools import setup
 setup(
     name='Augmentor',
     packages=['Augmentor'],  # this must be the same as the name above
-    version='0.2',
+    version='0.1',
     description='Image augmentation library for Machine Learning',
     license='MIT',
     author='Marcus D. Bloice',
     author_email='marcus.bloice@medunigraz.at',
     url='https://github.com/mdbloice/Augmentor',  # use the URL to the github repo
-    download_url='https://github.com/mdbloice/Augmentor/tarball/0.2',  #
+    download_url='https://github.com/mdbloice/Augmentor/tarball/0.1',  #
     keywords=['image', 'augmentation', 'artificial', 'generation', 'machine', 'learning'],  # arbitrary keywords
     classifiers=[
         'Development Status :: 4 - Beta',
