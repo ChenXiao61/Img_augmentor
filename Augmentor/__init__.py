f545b3018836bef69eb0112a2c866c1b87a9b96b
Marcus Bloice <marcus.bloice@medunigraz.at>
2016-08-19 13:27:15 +0200

    Removed older imports.

--- Augmentor/__init__.py
+++ Augmentor/__init__.py
@@ -1,18 +1,3 @@
-#from __future__ import unicode_literals
-#from __future__ import print_function
-#from __future__ import division
-#from __future__ import absolute_import
-#from future import standard_library
-#standard_library.install_aliases()
-#import os
-#from terminaltables import GithubFlavoredMarkdownTable
-#from PIL import Image, ImageOps
-#import random
-#import glob2
-#from collections import defaultdict
-#from fractions import gcd
-#from io import BytesIO
-
 from . import ImageDetails
 from . import ImageFormat
 from . import ImageOperations
