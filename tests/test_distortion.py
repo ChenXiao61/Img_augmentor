963386f36864662a9752f9189fef5c3bce3c804c
Marcus Bloice <marcus.bloice@medunigraz.at>
2017-06-09 13:44:09 +0200

    New utility functions for generating test images.

--- tests/test_distortion.py
+++ tests/test_distortion.py
@@ -9,14 +9,31 @@ import tempfile
 import io
 import shutil
 from PIL import Image
+from Augmentor import Operations
 
-from util_funcs import create_temp_JPEG
+from util_funcs import create_colour_temp_image, create_greyscale_temp_image
 
 def test_in_memory_distortions():
-    tmp, tmpdir = create_temp_JPEG((800, 800))
+    tmp, tmpdir = create_colour_temp_image((800, 800), "JPEG")
 
-    assert tmp is not None
-    assert tmpdir is not None
+    r_d = Operations.Distort(probability=1, grid_width=8, grid_height=8, magnitude=8)
+    tmp_im = Image.open(tmp)
+    tmp_im = r_d.perform_operation(tmp_im)
+
+    assert tmp_im is not None
+    assert tmp_im.size == (800,800)
+
+    tmp_bw, tmpdir_bw = create_greyscale_temp_image((800, 800), "PNG")
+
+    r_d_bw = Operations.Distort(probability=1, grid_width=8, grid_height=8, magnitude=8)
+    tmp_im_bw = Image.open(tmp_bw)
+    tmp_im_bw = r_d_bw.perform_operation(tmp_im_bw)
+
+    assert tmp_im_bw is not None
+    assert tmp_im_bw.size == (800,800)
+    assert isinstance(tmp_im_bw, Image.Image)
 
     tmp.close()
+    tmp_bw.close()
     shutil.rmtree(tmpdir)
+    shutil.rmtree(tmpdir_bw)
--- tests/util_funcs.py
+++ tests/util_funcs.py
@@ -3,13 +3,26 @@ import sys
 import tempfile
 import io
 import shutil
+import numpy as np
 
 from PIL import Image
 
-def create_temp_JPEG(size):
+
+def create_colour_temp_image(size, file_format):
     tmpdir = tempfile.mkdtemp()
     tmp = tempfile.NamedTemporaryFile(dir=tmpdir, suffix='.JPEG')
-    im = Image.new('RGB', size)
-    im.save(tmp.name, 'JPEG')
+
+    im = Image.fromarray(np.uint8(np.random.rand(800,800,3) * 255))
+    im.save(tmp.name, file_format)
+
+    return tmp, tmpdir
+
+
+def create_greyscale_temp_image(size, file_format):
+    tmpdir = tempfile.mkdtemp()
+    tmp = tempfile.NamedTemporaryFile(dir=tmpdir, suffix='.JPEG')
+
+    im = Image.fromarray(np.uint8(np.random.rand(800,800) * 255))
+    im.save(tmp.name, file_format)
 
     return tmp, tmpdir
