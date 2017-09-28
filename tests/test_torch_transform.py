919c45e6590e79d97685f3ee1948e86206370335
June Oh <me@juneoh.com>
2017-09-29 02:37:33 +0900

    Skip torch test if torchvision module is not installed

--- tests/test_torch_transform.py
+++ tests/test_torch_transform.py
@@ -3,11 +3,12 @@ import pytest
 # Context
 import numpy as np
 import PIL.Image as Image
-import torchvision
 
 import Augmentor
 
 def test_torch_transform():
+    torchvision = pytest.importorskip("torchvision")
+
     red = np.zeros([10, 10, 3], np.uint8)
     red[..., 0] = 255
     red = Image.fromarray(red)
@@ -20,4 +21,5 @@ def test_torch_transform():
         p.torch_transform()
     ])
 
+    assert red != transforms(red)
     assert g.perform_operation(red) == transforms(red)
